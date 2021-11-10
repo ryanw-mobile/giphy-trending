fd52ca39ac8f33e81948cd047e2e0e9283003229
Ryan Wong <ryan@rwmobi.com>
2021-11-10 21:00:29 +0000

    [refactor] Glide: update the BindingAdapter to perform additional url check; update the image configuration

--- app/src/main/java/uk/ryanwong/giphytrending/ui/BindingAdapter.kt
+++ app/src/main/java/uk/ryanwong/giphytrending/ui/BindingAdapter.kt
@@ -1,13 +1,22 @@
 package uk.ryanwong.giphytrending.ui
 
 import android.widget.ImageView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
 @BindingAdapter("imageUrl")
-fun setImageUrl(imageView: ImageView, url: String?) {
-    Glide.with(imageView)
-        .load(url)
-        .centerCrop()
-        .into(imageView)
+fun setImageUrl(imageView: ImageView, src: String?) {
+    src?.let {
+        val uri = src.toUri().buildUpon().scheme("https").build()
+        Glide.with(imageView)
+            .load(uri)
+//            .placeholder(R.drawable.ic_baseline_error_outline_24)
+//            .error(R.drawable.ic_baseline_error_outline_24)
+//            .fallback(R.drawable.ic_baseline_error_outline_24)
+            .into(imageView)
+    } ?: run {
+        // src is null, we still have to show a placeholder
+//        imageView.setImageResource(R.drawable.ic_baseline_error_outline_24)
+    }
 }

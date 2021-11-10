0f9290c9f3e3721fb0ac72419f178d65e27827ac
Ryan Wong <ryan@rwmobi.com>
2021-11-10 20:59:32 +0000

    [refactor] UI: update MainActivity and attach navigation host to the main view

--- app/src/main/java/uk/ryanwong/giphytrending/MainActivity.kt
+++ app/src/main/java/uk/ryanwong/giphytrending/MainActivity.kt
@@ -1,11 +1,14 @@
 package uk.ryanwong.giphytrending
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import uk.ryanwong.giphytrending.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

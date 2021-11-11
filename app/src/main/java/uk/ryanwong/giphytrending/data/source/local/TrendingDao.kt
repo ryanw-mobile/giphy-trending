d909f5804b74835be1a843bf82f57dd4bbf85f0a
Ryan Wong <ryan@rwmobi.com>
2021-11-11 19:13:36 +0000

    [feature] App: update list view ordering to sort by trending time desc AND import time desc

--- app/src/main/java/uk/ryanwong/giphytrending/data/source/local/TrendingDao.kt
+++ app/src/main/java/uk/ryanwong/giphytrending/data/source/local/TrendingDao.kt
@@ -15,7 +15,7 @@ interface TrendingDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAllData(data: List<TrendingEntity>)
 
-    @Query("SELECT * FROM trending ORDER BY import_datetime DESC")
+    @Query("SELECT * FROM trending ORDER BY trending_datetime DESC, import_datetime DESC")
     fun queryData(): Single<List<TrendingEntity>>
 
     @Query("DELETE FROM trending")

b58552b5b7b3deeca1b8c50995f02b2576a295e4
Ryan Wong <ryan@rwmobi.com>
2021-11-14 04:22:58 +0000

    [refactor] TrendingRepository: rename to GiphyRepository and rename LiveData variable names

--- app/src/main/java/uk/ryanwong/giphytrending/data/repository/GiphyRepository.kt
+++ app/src/main/java/uk/ryanwong/giphytrending/data/repository/GiphyRepository.kt
@@ -9,33 +9,36 @@ import io.reactivex.subscribers.DisposableSubscriber
 import timber.log.Timber
 import uk.ryanwong.giphytrending.BuildConfig
 import uk.ryanwong.giphytrending.GiphyApplication
+import uk.ryanwong.giphytrending.data.source.local.GiphyDatabase
 import uk.ryanwong.giphytrending.data.source.local.toDomainModelList
 import uk.ryanwong.giphytrending.data.source.local.toTrendingEntityList
 import uk.ryanwong.giphytrending.data.source.network.GiphyApi
 import uk.ryanwong.giphytrending.data.source.network.model.TrendingNetworkModel
-import uk.ryanwong.giphytrending.di.DaggerAppComponent
 import uk.ryanwong.giphytrending.domain.model.GiphyImageItemDomainModel
 import javax.inject.Inject
 
-class TrendingRepository {
+class GiphyRepository {
 
     @Inject
     lateinit var giphyApiService: GiphyApi
+
+    @Inject
+    lateinit var giphyDatabase: GiphyDatabase
 
     private val _trendingList by lazy { MutableLiveData<List<GiphyImageItemDomainModel>>() }
     val trendingList: LiveData<List<GiphyImageItemDomainModel>>
         get() = _trendingList
 
-    private val _isInProgress by lazy { MutableLiveData<Boolean>() }
-    val isInProgress: LiveData<Boolean>
-        get() = _isInProgress
+    private val _showLoading by lazy { MutableLiveData<Boolean>() }
+    val showLoading: LiveData<Boolean>
+        get() = _showLoading
 
-    private val _isError by lazy { MutableLiveData<String>() }
-    val isError: LiveData<String>
-        get() = _isError
+    private val _errorMessage by lazy { MutableLiveData<String>() }
+    val errorMessage: LiveData<String>
+        get() = _errorMessage
 
     init {
-        DaggerAppComponent.create().inject(this)
+        GiphyApplication.appComponent.inject(this)
     }
 
     /***
@@ -44,19 +47,19 @@ class TrendingRepository {
      * This can be changed without acknowledging to the callers.
      */
     fun fetchTrending(): Disposable {
-        return GiphyApplication.database.trendingDao()
+        return giphyDatabase.trendingDao()
             .queryData()
             .subscribeOn(Schedulers.io())
             .subscribe(
                 { dataEntityList ->
                     if (dataEntityList != null) {
-                        _isError.postValue(null)
+                        _errorMessage.postValue(null)
                         _trendingList.postValue(dataEntityList.toDomainModelList())
                     }
                 },
                 {
                     Timber.e("fetchTrending() - Database error: ${it.message}")
-                    _isError.postValue(
+                    _errorMessage.postValue(
                         it?.message ?: "Error when retrieving data from the local storage."
                     )
                 }
@@ -70,8 +73,9 @@ class TrendingRepository {
      */
     fun refreshTrending(): Disposable {
         return Completable.fromAction {
+            _showLoading.postValue(true)
             // Mark existing contents dirty. After successful API call old entries will be removed
-            GiphyApplication.database.trendingDao().markDirty()
+            giphyDatabase.trendingDao().markDirty()
         }.subscribeOn(Schedulers.io())
             .subscribe(
                 {
@@ -79,7 +83,10 @@ class TrendingRepository {
                     getTrendingFromNetwork()
                 }, {
                     Timber.e("refreshTrending() -Database error when marking dirty bit: ${it.message}")
-                    _isError.postValue(it?.message ?: "Error when accessing to the local storage.")
+                    _showLoading.postValue(false)
+                    _errorMessage.postValue(
+                        it?.message ?: "Error when accessing to the local storage."
+                    )
                 }
             )
     }
@@ -98,17 +105,16 @@ class TrendingRepository {
             override fun onNext(trending: TrendingNetworkModel?) {
                 if (trending != null) {
                     val dataList = trending.trendingData.toTrendingEntityList()
-                    GiphyApplication.database.trendingDao().insertAllData(dataList)
+                    giphyDatabase.trendingDao().insertAllData(dataList)
                 }
             }
 
             override fun onError(t: Throwable?) {
-                _isInProgress.postValue(true)
                 Timber.e("cacheTrendingToDb(): error - ${t?.message}")
-                _isError.postValue(
+                _errorMessage.postValue(
                     t?.message ?: "Error when processing data returned from the server."
                 )
-                _isInProgress.postValue(false)
+                _showLoading.postValue(false)
             }
 
             override fun onComplete() {
@@ -120,14 +126,16 @@ class TrendingRepository {
 
     private fun invalidateDirtyTrendingDb(): Disposable {
         return Completable.fromAction {
-            GiphyApplication.database.trendingDao().deleteDirty()
+            giphyDatabase.trendingDao().deleteDirty()
         }.subscribeOn(Schedulers.io())
             .subscribe({
                 Timber.v("invalidateDirtyTrendingDb(): success")
                 fetchTrending()
+                _showLoading.postValue(false)
             },
                 {
                     Timber.e("invalidateDirtyTrendingDb: error when invalidating dirty rows - ${it?.message}")
+                    _showLoading.postValue(false)
                 })
     }
 }

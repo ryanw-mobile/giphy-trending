9d09c9abbd965f2ea41c0f05a36e8947debbbec7
Ryan Wong <ryan@rwmobi.com>
2021-11-14 04:20:42 +0000

    [refactor] RoomDB: simplify RoomDB DI initialisation

--- app/src/main/java/uk/ryanwong/giphytrending/data/source/local/GiphyDatabase.kt
+++ app/src/main/java/uk/ryanwong/giphytrending/data/source/local/GiphyDatabase.kt
@@ -1,33 +1,11 @@
 package uk.ryanwong.giphytrending.data.source.local
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import uk.ryanwong.giphytrending.BuildConfig
 
 @Database(entities = [TrendingEntity::class], version = 5, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class GiphyDatabase : RoomDatabase() {
     abstract fun trendingDao(): TrendingDao
-
-    companion object {
-
-        @Volatile // All threads have immediate access to this property
-        private var instance: GiphyDatabase? = null
-
-        private val LOCK = Any() // Makes sure no threads making the same thing at the same time
-
-        private fun buildDatabase(context: Context) =
-            Room.databaseBuilder(
-                context.applicationContext,
-                GiphyDatabase::class.java,
-                BuildConfig.DATABASE_NAME
-            ).fallbackToDestructiveMigration().build()
-
-        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
-            instance ?: buildDatabase(context).also { instance = it }
-        }
-    }
 }
--- app/src/main/java/uk/ryanwong/giphytrending/di/AppModule.kt
+++ app/src/main/java/uk/ryanwong/giphytrending/di/AppModule.kt
@@ -1,26 +1,40 @@
 package uk.ryanwong.giphytrending.di
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
-import uk.ryanwong.giphytrending.data.repository.TrendingRepository
+import uk.ryanwong.giphytrending.BuildConfig
+import uk.ryanwong.giphytrending.data.repository.GiphyRepository
+import uk.ryanwong.giphytrending.data.source.local.GiphyDatabase
 import uk.ryanwong.giphytrending.data.source.network.GiphyApi
 import uk.ryanwong.giphytrending.data.source.network.GiphyApiService
-import uk.ryanwong.giphytrending.domain.model.GiphyImageItemDomainModel
 import uk.ryanwong.giphytrending.ui.GiphyImageItemAdapter
 import javax.inject.Singleton
 
 @Module
-class AppModule {
+class AppModule(val context: Context) {
+
+    @Singleton
+    @Provides
+    fun provideContext(): Context {
+        return context
+    }
+
+    @Singleton
+    @Provides
+    fun provideDatabase(context: Context): GiphyDatabase = Room.databaseBuilder(
+        context,
+        GiphyDatabase::class.java,
+        BuildConfig.DATABASE_NAME
+    ).fallbackToDestructiveMigration().build()
 
     @Singleton
     @Provides
     fun provideApi(): GiphyApi = GiphyApiService.getClient()
 
     @Provides
-    fun provideTrendingRepository() = TrendingRepository()
-
-    @Provides
-    fun provideTrendingList() = ArrayList<GiphyImageItemDomainModel>()
+    fun provideTrendingRepository() = GiphyRepository()
 
     @Provides
     fun provideTrendingAdapter(): GiphyImageItemAdapter = GiphyImageItemAdapter().apply {

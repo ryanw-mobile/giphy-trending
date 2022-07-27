1fca243c2f569c6ea6538956627276658753f28a
Ryan Wong <ryan@rwmobi.com>
2022-07-27 22:29:34 +0100

    [refactor] SettingsFragment and SettingsViewModel: polished to use Hilt, Coroutines, and Flow

--- app/src/main/java/uk/ryanwong/giphytrending/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/uk/ryanwong/giphytrending/ui/settings/SettingsViewModel.kt
@@ -1,30 +1,38 @@
 package uk.ryanwong.giphytrending.ui.settings
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.map
-import uk.ryanwong.giphytrending.GiphyApplication
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import uk.ryanwong.giphytrending.data.repository.UserPreferencesRepository
+import uk.ryanwong.giphytrending.di.MainDispatcher
 import javax.inject.Inject
-import kotlin.math.max
 
 // Minimum entries to request from server
 private const val API_MIN = 50
 
-class SettingsViewModel @Inject constructor(private val repository: UserPreferencesRepository) :
-    ViewModel() {
+@HiltViewModel
+class SettingsViewModel @Inject constructor(
+    private val repository: UserPreferencesRepository,
+    @MainDispatcher private val dispatcher: CoroutineDispatcher
+) : ViewModel() {
+
+    private val _apiMaxEntriesProgress: MutableStateFlow<Int> = MutableStateFlow(0)
+    var apiMaxEntriesProgress: StateFlow<Int> = _apiMaxEntriesProgress
 
     // The seekbar progress has to deduct the minimum value
-    val apiMaxEntriesProgress = repository.apiMaxEntries.map { apiMax ->
-        max(apiMax.minus(API_MIN), 0)
-    }
-
-    init {
-        GiphyApplication.appComponent.inject(this)
-    }
+//    val apiMaxEntriesProgress = repository.apiMaxEntries.map { apiMax ->
+//        max(apiMax.minus(API_MIN), 0)
+//    }
 
     fun translateMaxApiEntries(value: Int) = value.plus(API_MIN).toString()
 
     fun setApiMax(maxApiEntries: Int) {
-        repository.updateApiMax(maxApiEntries.plus(API_MIN))
+        viewModelScope.launch(dispatcher) {
+            repository.setApiMax(maxApiEntries.plus(API_MIN))
+        }
     }
 }

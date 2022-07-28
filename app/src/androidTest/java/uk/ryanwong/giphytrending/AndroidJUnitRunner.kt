8d90e94791b8b47f1c44cb8fd7477d3c54631450
Ryan Wong <ryan@rwmobi.com>
2022-07-28 16:48:50 +0100

    [chore] gradle: changes to make unit test build to work

---
+++ app/src/androidTest/java/uk/ryanwong/giphytrending/AndroidJUnitRunner.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021. Ryan Wong (hello@ryanwong.co.uk)
+ *
+ */
+
+package uk.ryanwong.dazn.codechallenge
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class CustomTestRunner : AndroidJUnitRunner() {
+
+    override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}

dbc8c3e293dfb525cdbab0db6cafe977c9c42f9b
Ryan Wong <ryan@rwmobi.com>
2022-07-28 16:49:21 +0100

    [test] unit test: comment out blank unit tests

---
+++ app/src/test/java/uk/ryanwong/giphytrending/data/repository/GiphyRepositoryImplTest.kt
@@ -0,0 +1,17 @@
+package uk.ryanwong.giphytrending.data.repository
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+// TODO: rewrite in kotest format
+class GiphyRepositoryImplTest {
+//
+//    @Test
+//    fun fetchCachedTrending() {
+//    }
+//
+//    @Test
+//    fun reloadTrending() {
+//    }
+}

1cc257746b0f789be4b656252122e94154d5917c
Ryan Wong <ryan@rwmobi.com>
2022-07-29 16:19:12 +0100

    [chore] TrendingNetworkResponse: add comment

--- app/src/main/java/uk/ryanwong/giphytrending/data/source/network/model/TrendingNetworkResponse.kt
+++ app/src/main/java/uk/ryanwong/giphytrending/data/source/network/model/TrendingNetworkResponse.kt
@@ -3,6 +3,14 @@ package uk.ryanwong.giphytrending.data.source.network.model
 import com.squareup.moshi.Json
 import java.util.Date
 
+/***
+ * Note: Obviously large chunk of data is not being used by this App for the moment,
+ * and can be removed. However this is a demo app which I do not have a firmed scope here,
+ * leaving it there might help me quickly get new ideas on what else to app to this App.
+ *
+ * Otherwise in a real project once we know what's required, we don't have to keep so much,
+ * as they will be thrown away at the time we convert it to domain model anyway.
+ */
 data class TrendingNetworkResponse(
     @Json(name = "data")
     val trendingData: List<TrendingData>,

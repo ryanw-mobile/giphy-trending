fb7eb269240846eb4cd73f5e5aff9dcc1ea9c29b
Ryan Wong <ryan@rwmobi.com>
2023-03-26 10:15:04 +0100

    [refactor] Gradle: migrate groovy to Kotlin DSL (WIP)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        jcenter() // Warning: this repository is going to shut down soon
+        maven("https://jitpack.io")
     }
 }
+
 rootProject.name = "Giphy Trending"
-include ':app'
-include ':benchmark'
+include("app")
+include("benchmark")

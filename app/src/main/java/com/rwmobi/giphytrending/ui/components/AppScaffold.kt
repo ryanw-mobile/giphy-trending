08a2edab73b7317159042e0eb632b4322e3ed80c
Ryan Wong <ryan@rwmobi.com>
2024-04-08 03:11:03 +0100

    [feature] App: set snackbar duration to long instead of indefinite

--- app/src/main/java/com/rwmobi/giphytrending/ui/components/AppScaffold.kt
+++ app/src/main/java/com/rwmobi/giphytrending/ui/components/AppScaffold.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
@@ -88,6 +89,7 @@ fun AppScaffold(modifier: Modifier = Modifier) {
                     snackbarHostState.showSnackbar(
                         message = errorMessageText,
                         actionLabel = actionLabel,
+                        duration = SnackbarDuration.Long,
                     )
                 },
             )

824dfbc031e9e67ac38c99ef58a3e3cd5520b09a
Ryan Wong <ryan@rwmobi.com>
2024-04-19 19:25:11 +0100

    [test] FakeNetworkDataSourceB.kt: add search to make tests run again

--- app/src/androidTest/java/com/rwmobi/giphytrending/data/source/network/FakeNetworkDataSourceB.kt
+++ app/src/androidTest/java/com/rwmobi/giphytrending/data/source/network/FakeNetworkDataSourceB.kt
@@ -5,12 +5,14 @@
 
 package com.rwmobi.giphytrending.data.source.network
 
+import com.rwmobi.giphytrending.data.source.network.dto.SearchNetworkResponseDto
 import com.rwmobi.giphytrending.data.source.network.dto.TrendingNetworkResponseDto
 import com.rwmobi.giphytrending.data.source.network.interfaces.NetworkDataSource
 
 class FakeNetworkDataSourceB : NetworkDataSource {
     var apiError: Throwable? = null
-    var mockTrendingNetworkResponseDto: TrendingNetworkResponseDto? = null
+    var trendingNetworkResponseDto: TrendingNetworkResponseDto? = null
+    var searchNetworkResponseDto: SearchNetworkResponseDto? = null
 
     override suspend fun getTrending(
         apiKey: String,
@@ -19,6 +21,11 @@ class FakeNetworkDataSourceB : NetworkDataSource {
         rating: String,
     ): TrendingNetworkResponseDto {
         apiError?.run { throw this }
-        return mockTrendingNetworkResponseDto ?: SampleTrendingNetworkResponseDto.trendingNetworkResponseDto
+        return trendingNetworkResponseDto ?: SampleTrendingNetworkResponseDto.trendingNetworkResponseDto
+    }
+
+    override suspend fun getSearch(apiKey: String, keyword: String, limit: Int, offset: Int, rating: String): SearchNetworkResponseDto {
+        apiError?.run { throw this }
+        return searchNetworkResponseDto ?: SampleSearchNetworkResponseDto.searchNetworkResponseDto
     }
 }

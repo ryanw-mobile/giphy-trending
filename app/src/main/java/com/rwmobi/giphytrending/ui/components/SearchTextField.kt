576a4ed04f0218314c85cf24d48ee5fce7006e8c
Ryan Wong <ryan@rwmobi.com>
2024-04-19 17:46:37 +0100

    [feature] Search: implement search UI

---
+++ app/src/main/java/com/rwmobi/giphytrending/ui/components/SearchTextField.kt
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2024. Ryan Wong
+ * https://github.com/ryanw-mobile
+ */
+
+package com.rwmobi.giphytrending.ui.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import com.rwmobi.giphytrending.R
+import com.rwmobi.giphytrending.ui.theme.GiphyTrendingTheme
+import com.rwmobi.giphytrending.ui.theme.getDimension
+import kotlinx.coroutines.launch
+
+@Composable
+fun SearchTextField(
+    modifier: Modifier = Modifier,
+    keyword: String,
+    focusManager: FocusManager,
+    onUpdateKeyword: (keyword: String) -> Unit,
+    onClearKeyword: () -> Unit,
+    onSearch: () -> Unit,
+) {
+    val dimension = LocalConfiguration.current.getDimension()
+    val focusRequester = FocusRequester()
+    val coroutineScope = rememberCoroutineScope()
+    var isFocused by remember { mutableStateOf(false) }
+
+    TextField(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimension.defaultHalfPadding)
+            .focusRequester(focusRequester)
+            .onFocusChanged { focusState ->
+                isFocused = focusState.isFocused
+            },
+        value = keyword,
+        onValueChange = { onUpdateKeyword(it) },
+        singleLine = true,
+        maxLines = 1,
+        shape = MaterialTheme.shapes.medium,
+        colors = TextFieldDefaults.colors().copy(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            cursorColor = MaterialTheme.colorScheme.onPrimaryContainer.copy(
+                alpha = 0.28f,
+            ),
+        ),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Search,
+        ),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                onSearch()
+                coroutineScope.launch {
+                    focusManager.clearFocus()
+                }
+            },
+        ),
+        placeholder = {
+            Text(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .fillMaxWidth(),
+                maxLines = 1,
+                style = MaterialTheme.typography.bodyMedium,
+                text = stringResource(id = R.string.search),
+            )
+        },
+        leadingIcon = {
+            Icon(
+                modifier = Modifier.wrapContentSize(),
+                imageVector = Icons.Filled.Search,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onPrimaryContainer.copy(
+                    alpha = 0.28f,
+                ),
+            )
+        },
+        trailingIcon = {
+            AnimatedVisibility(
+                visible = isFocused && keyword.isNotEmpty(),
+                enter = fadeIn(),
+                exit = fadeOut(),
+            ) {
+                IconButton(onClick = onClearKeyword) {
+                    Icon(
+                        modifier = Modifier.wrapContentSize(),
+                        imageVector = Icons.Filled.Clear,
+                        contentDescription = stringResource(R.string.content_description_clear_search_keyword),
+                        tint = MaterialTheme.colorScheme.onPrimaryContainer.copy(
+                            alpha = 0.68f,
+                        ),
+                    )
+                }
+            }
+        },
+    )
+}
+
+@Preview
+@Composable
+private fun SearchTextFieldEmptyPreview() {
+    GiphyTrendingTheme {
+        Surface {
+            SearchTextField(
+                keyword = "",
+                focusManager = LocalFocusManager.current,
+                onUpdateKeyword = {},
+                onClearKeyword = {},
+                onSearch = { },
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun SearchTextFieldPreview() {
+    GiphyTrendingTheme {
+        Surface {
+            SearchTextField(
+                keyword = "some keyword which is pretty long but still within the limit",
+                focusManager = LocalFocusManager.current,
+                onUpdateKeyword = {},
+                onClearKeyword = {},
+                onSearch = { },
+            )
+        }
+    }
+}
--- app/src/main/java/com/rwmobi/giphytrending/ui/destinations/search/SearchScreen.kt
+++ app/src/main/java/com/rwmobi/giphytrending/ui/destinations/search/SearchScreen.kt
@@ -5,47 +5,42 @@
 
 package com.rwmobi.giphytrending.ui.destinations.search
 
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.TextField
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalClipboardManager
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.input.ImeAction
 import androidx.core.net.toUri
 import coil.ImageLoader
 import com.rwmobi.giphytrending.R
 import com.rwmobi.giphytrending.ui.components.NoDataScreen
+import com.rwmobi.giphytrending.ui.components.SearchTextField
 import com.rwmobi.giphytrending.ui.destinations.trendinglist.TrendingListCompact
 import com.rwmobi.giphytrending.ui.destinations.trendinglist.TrendingStaggeredGrid
-import com.rwmobi.giphytrending.ui.theme.getDimension
 import com.rwmobi.giphytrending.ui.utils.downloadImage
 import com.rwmobi.giphytrending.ui.utils.startBrowserActivity
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen(
     modifier: Modifier = Modifier,
@@ -67,27 +62,28 @@ fun SearchScreen(
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
     val clipboardHistory = remember { mutableStateListOf<String>() }
-    val dimension = LocalConfiguration.current.getDimension()
+    val focusManager: FocusManager = LocalFocusManager.current
     val nestedScrollConnection = rememberNestedScrollInteropConnection()
 
-    Box(modifier = modifier.nestedScroll(connection = nestedScrollConnection)) {
+    Box(
+        modifier = modifier
+            .nestedScroll(connection = nestedScrollConnection)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = { coroutineScope.launch { focusManager.clearFocus() } },
+                )
+            },
+    ) {
         Column(
             modifier = Modifier.fillMaxSize(),
         ) {
-            var tempKeyword by remember { mutableStateOf(uiState.keyword) }
-
-            TextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = dimension.defaultFullPadding),
-                value = tempKeyword,
-                onValueChange = { tempKeyword = it },
-                singleLine = true,
-                maxLines = 1,
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = ImeAction.Search,
-                ),
-                keyboardActions = KeyboardActions(onSearch = { uiEvent.onSearch(tempKeyword) }),
+            SearchTextField(
+                modifier = Modifier.fillMaxWidth(),
+                keyword = uiState.keyword,
+                focusManager = focusManager,
+                onClearKeyword = uiEvent.onClearKeyword,
+                onUpdateKeyword = { uiEvent.onUpdateKeyword(it.take(uiState.keywordMaxLength)) },
+                onSearch = uiEvent.onSearch,
             )
 
             uiState.giphyImageItems?.let { giphyImageItems ->
@@ -141,6 +137,15 @@ fun SearchScreen(
                             .fillMaxSize()
                             .verticalScroll(rememberScrollState()), // to support pull to refresh
                     )
+                }
+            }
+        }
+
+        DisposableEffect(true) {
+            onDispose {
+                uiEvent.onClearKeyword
+                coroutineScope.launch {
+                    focusManager.clearFocus()
                 }
             }
         }

afdfe322528fa4cfb6e228d7b11b779bcdcf3b91
Ryan Wong <ryan@rwmobi.com>
2024-04-26 19:13:41 +0100

    [test] RatingTest: rename unit test functions

--- app/src/test/java/com/rwmobi/giphytrending/domain/model/RatingTest.kt
+++ app/src/test/java/com/rwmobi/giphytrending/domain/model/RatingTest.kt
@@ -1,11 +1,19 @@
+/*
+ * Copyright (c) 2024. Ryan Wong
+ * https://github.com/ryanw-mobile
+ */
+
 package com.rwmobi.giphytrending.domain.model
 
 import io.kotest.matchers.shouldBe
 import org.junit.Test
 
 class RatingTest {
+
+    // Test function names reviewed by ChatGPT for consistency
+
     @Test
-    fun `fromApiValue should return the correct enum for valid inputs when input is lowercase`() {
+    fun fromApiValue_ShouldReturnCorrectEnum_ForValidLowercaseInputs() {
         Rating.fromApiValue("g", Rating.PG) shouldBe Rating.G
         Rating.fromApiValue("pg", Rating.G) shouldBe Rating.PG
         Rating.fromApiValue("pg-13", Rating.G) shouldBe Rating.PG_13
@@ -13,7 +21,7 @@ class RatingTest {
     }
 
     @Test
-    fun `fromApiValue should return the correct enum for valid inputs when input is uppercase`() {
+    fun fromApiValue_ShouldReturnCorrectEnum_ForValidUppercaseInputs() {
         Rating.fromApiValue("G", Rating.PG) shouldBe Rating.G
         Rating.fromApiValue("PG", Rating.G) shouldBe Rating.PG
         Rating.fromApiValue("PG-13", Rating.G) shouldBe Rating.PG_13
@@ -21,31 +29,31 @@ class RatingTest {
     }
 
     @Test
-    fun `fromApiValue should return default value for invalid inputs when input is an unknown rating`() {
+    fun fromApiValue_ShouldReturnDefaultValue_ForUnknownRatingInputs() {
         val defaultValue = Rating.G
         Rating.fromApiValue("x", defaultValue) shouldBe defaultValue
     }
 
     @Test
-    fun `fromApiValue should return default value for invalid inputs when input is an incorrectly formatted rating`() {
+    fun fromApiValue_ShouldReturnDefaultValue_ForIncorrectlyFormattedRatingInputs() {
         val defaultValue = Rating.G
         Rating.fromApiValue("pg13", defaultValue) shouldBe defaultValue
     }
 
     @Test
-    fun `fromApiValue should return default value for invalid inputs when input is an empty string`() {
+    fun fromApiValue_ShouldReturnDefaultValue_ForEmptyStringInput() {
         val defaultValue = Rating.G
         Rating.fromApiValue("", defaultValue) shouldBe defaultValue
     }
 
     @Test
-    fun `fromApiValue should return default value for invalid inputs when input is only whitespace`() {
+    fun fromApiValue_ShouldReturnDefaultValue_ForWhitespaceOnlyInput() {
         val defaultValue = Rating.G
         Rating.fromApiValue(" ", defaultValue) shouldBe defaultValue
     }
 
     @Test
-    fun `fromApiValue should handle null input by returning default value`() {
+    fun fromApiValue_ShouldReturnDefaultValue_ForNullInput() {
         val defaultValue = Rating.G
         Rating.fromApiValue(null, defaultValue) shouldBe defaultValue
     }

870c03e07718022e2a051cf26d86041d20527300
Ryan Wong <ryan@rwmobi.com>
2024-04-26 19:05:20 +0100

    [test] TrendingDaoTest: rename unit test functions

--- app/src/test/java/com/rwmobi/giphytrending/data/source/local/dao/TrendingDaoTest.kt
+++ app/src/test/java/com/rwmobi/giphytrending/data/source/local/dao/TrendingDaoTest.kt
@@ -47,132 +47,108 @@ class TrendingDaoTest {
     }
 
     // Basic test cases: CRUD usages
-    @Test
-    fun emptyDatabase_InsertOne_ReturnOne() =
-        runTest {
-            trendingDao.queryData() shouldHaveSize 0
-
-            trendingDao.insertData(SampleTrendingEntity.case1)
-
-            trendingDao.queryData() shouldContainExactly (listOf(SampleTrendingEntity.case1))
-        }
+    // Test function names reviewed by ChatGPT for consistency
 
     @Test
-    fun emptyDatabase_InsertList_ReturnSameList() =
-        runTest {
-            trendingDao.queryData() shouldHaveSize 0
-
-            val testTrendingList = listOf(
-                SampleTrendingEntity.case2,
-                SampleTrendingEntity.case3,
-                SampleTrendingEntity.case4,
-            )
-            trendingDao.insertAllData(testTrendingList)
-
-            // Note: The order may not be the same due to sorting
-            trendingDao.queryData() shouldContainAll (testTrendingList)
-        }
+    fun insertData_WithSingleEntry_ShouldReturnSingleEntry() = runTest {
+        trendingDao.insertData(SampleTrendingEntity.case1)
+        trendingDao.queryData() shouldContainExactly (listOf(SampleTrendingEntity.case1))
+    }
 
     @Test
-    fun nonEmptyDatabase_UpdateOneRow_ReturnUpdatedList() =
-        runTest {
-            val testTrendingList = listOf(
-                SampleTrendingEntity.case1,
-                SampleTrendingEntity.case2,
-                SampleTrendingEntity.case3,
-            )
-            trendingDao.insertAllData(testTrendingList)
-            trendingDao.queryData() shouldHaveSize 3
+    fun insertAllData_WithMultipleEntries_ShouldMatchInsertedEntries() = runTest {
+        val testTrendingList = listOf(
+            SampleTrendingEntity.case2,
+            SampleTrendingEntity.case3,
+            SampleTrendingEntity.case4,
+        )
 
-            trendingDao.insertData(SampleTrendingEntity.case2Modified)
+        trendingDao.insertAllData(testTrendingList)
 
-            val trendingList = trendingDao.queryData()
-            trendingList shouldHaveSize 3
-            trendingList shouldContain SampleTrendingEntity.case1
-            trendingList shouldNotContain SampleTrendingEntity.case2
-            trendingList shouldContain SampleTrendingEntity.case2Modified
-            trendingList shouldContain SampleTrendingEntity.case3
-        }
+        // The order may not be the same due to sorting
+        trendingDao.queryData() shouldContainAll (testTrendingList)
+    }
 
     @Test
-    fun nonemptyDatabase_ClearDatabase_ReturnEmptyList() =
-        runTest {
-            val testTrendingList = listOf(
-                SampleTrendingEntity.case1,
-                SampleTrendingEntity.case2,
-                SampleTrendingEntity.case3,
-            )
-            trendingDao.insertAllData(testTrendingList)
-            trendingDao.queryData() shouldHaveSize 3
+    fun updateData_WithModifiedEntry_ShouldReflectUpdatedEntry() = runTest {
+        val testTrendingList = listOf(
+            SampleTrendingEntity.case1,
+            SampleTrendingEntity.case2,
+            SampleTrendingEntity.case3,
+        )
+        trendingDao.insertAllData(testTrendingList)
 
-            trendingDao.clear()
+        trendingDao.insertData(SampleTrendingEntity.case2Modified)
 
-            trendingDao.queryData() shouldHaveSize 0
-        }
-
-    /***
-     * Dirty bit test cases
-     */
-    @Test
-    fun nonEmptyCleanDatabase_markDirty_ReturnAllDirty() =
-        runTest {
-            val testTrendingList = listOf(
-                SampleTrendingEntity.case1,
-                SampleTrendingEntity.case2,
-                SampleTrendingEntity.case3,
-            )
-            trendingDao.insertAllData(testTrendingList)
-            val initialList = trendingDao.queryData()
-            initialList shouldHaveSize 3
-            initialList[0].dirty shouldBe false
-            initialList[1].dirty shouldBe false
-            initialList[2].dirty shouldBe false
-
-            trendingDao.markDirty()
-
-            val trendingList = trendingDao.queryData()
-            trendingList shouldHaveSize 3
-            trendingList[0].dirty shouldBe true
-            trendingList[1].dirty shouldBe true
-            trendingList[2].dirty shouldBe true
-        }
+        val trendingList = trendingDao.queryData()
+        trendingList shouldHaveSize 3
+        trendingList shouldContain SampleTrendingEntity.case1
+        trendingList shouldNotContain SampleTrendingEntity.case2
+        trendingList shouldContain SampleTrendingEntity.case2Modified
+        trendingList shouldContain SampleTrendingEntity.case3
+    }
 
     @Test
-    fun allDirtyDatabase_deleteDirty_ReturnEmpty() =
-        runTest {
-            val testTrendingList = listOf(
-                SampleTrendingEntity.case1,
-                SampleTrendingEntity.case2,
-                SampleTrendingEntity.case3,
-            )
-            trendingDao.insertAllData(testTrendingList)
-            trendingDao.markDirty()
-            val initialList = trendingDao.queryData()
-            initialList shouldHaveSize 3
-            initialList[0].dirty shouldBe true
-            initialList[1].dirty shouldBe true
-            initialList[2].dirty shouldBe true
+    fun clearDatabase_WhenCalled_ShouldResultInEmptyDatabase() = runTest {
+        val testTrendingList = listOf(
+            SampleTrendingEntity.case1,
+            SampleTrendingEntity.case2,
+            SampleTrendingEntity.case3,
+        )
+        trendingDao.insertAllData(testTrendingList)
 
-            trendingDao.deleteDirty()
+        trendingDao.clear()
 
-            trendingDao.queryData() shouldHaveSize 0
-        }
+        trendingDao.queryData() shouldHaveSize 0
+    }
+
+    // Dirty bit test cases
+    @Test
+    fun markDirty_OnCleanDatabase_ShouldSetAllEntriesAsDirty() = runTest {
+        val testTrendingList = listOf(
+            SampleTrendingEntity.case1,
+            SampleTrendingEntity.case2,
+            SampleTrendingEntity.case3,
+        )
+        trendingDao.insertAllData(testTrendingList)
+
+        trendingDao.markDirty()
+
+        val trendingList = trendingDao.queryData()
+        trendingList shouldHaveSize 3
+        trendingList[0].dirty shouldBe true
+        trendingList[1].dirty shouldBe true
+        trendingList[2].dirty shouldBe true
+    }
 
     @Test
-    fun someDirtyDatabase_deleteDirty_ReturnClean() =
-        runTest {
-            val testTrendingList = listOf(
-                SampleTrendingEntity.case1,
-                SampleTrendingEntity.case2,
-                SampleTrendingEntity.case3,
-            )
-            trendingDao.insertAllData(testTrendingList)
-            trendingDao.markDirty()
-            trendingDao.insertData(SampleTrendingEntity.case4)
-            trendingDao.queryData() shouldHaveSize 4
+    fun deleteDirty_WhenAllEntriesAreDirty_ShouldClearDatabase() = runTest {
+        val testTrendingList = listOf(
+            SampleTrendingEntity.case1,
+            SampleTrendingEntity.case2,
+            SampleTrendingEntity.case3,
+        )
+        trendingDao.insertAllData(testTrendingList)
+        trendingDao.markDirty()
 
-            trendingDao.deleteDirty()
+        trendingDao.deleteDirty()
 
-            trendingDao.queryData() shouldContainExactly (listOf(SampleTrendingEntity.case4))
-        }
+        trendingDao.queryData() shouldHaveSize 0
+    }
+
+    @Test
+    fun deleteDirty_WithMixedCleanAndDirtyEntries_ShouldRemoveOnlyDirtyEntries() = runTest {
+        val testTrendingList = listOf(
+            SampleTrendingEntity.case1,
+            SampleTrendingEntity.case2,
+            SampleTrendingEntity.case3,
+        )
+        trendingDao.insertAllData(testTrendingList)
+        trendingDao.markDirty()
+        trendingDao.insertData(SampleTrendingEntity.case4)
+
+        trendingDao.deleteDirty()
+
+        trendingDao.queryData() shouldContainExactly (listOf(SampleTrendingEntity.case4))
+    }
 }
